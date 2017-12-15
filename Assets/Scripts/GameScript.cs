48a966365cdeeeca8dddddb481956af502ed0fcb
Julia Cho <julia.cho@zemind.ca>
2017-12-15 10:21:31 -0500

    set up basic

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScript : MonoBehaviour {
+	public bool isGameStarted;
+	private float timeLimit = 60.0f;
+	private float timeLeft;
+	public int score;
+	public GameObject[] holes;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		isGameStarted = true;
+		timeLeft = timeLimit;
+		score = 0;
+		holes = GameObject.FindGameObjectsWithTag ("hole");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isGameStarted) {
+			timeLeft -= Time.deltaTime;
+
+			if (timeLeft <= 0.0f) {
+				isGameStarted = false;
+				EndGame ();
+			}
+
+		}
+
 	}
+
+	public void ChangeScore (bool isPlus) {
+		if (isPlus)
+			score += 10;
+		else
+			score -= 5;
+	}
+
+	private void EndGame() {
+		// TODO
+	}
+
+
 }
---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour {
+	public int num;
+	public bool isLeftHalf;
+
+	private bool inited;		// Is initialized? (called from Game Script?)
+	private bool amIActive;		// Fish popping up?
+
+	private bool isKoi;			// Koi or Piranha
+
+
+	private float nextGenTimer;
+	private float timerMax;
+	private float timerMin;
+
+	// Use this for initialization
+	void Start () {
+		inited = false;
+		amIActive = false;
+		isKoi = false;
+
+		nextGenTimer = 0.0f;
+		timerMax = 10.0f;
+		timerMin = 3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inited) {
+			nextGenTimer -= Time.deltaTime;
+
+			if (nextGenTimer <= 0) {
+				activateHole();
+			}
+		}
+	}
+
+	public void Touched() {
+		if (amIActive) {
+		// TODO: Change the display
+		}
+	}
+
+	public void activateHole() {
+		if (!amIActive) {
+			System.Random rand = new System.Random();
+			int generateKoi = rand.Next (4);
+			if (generateKoi == 0) {
+				// Generate Piranha
+			} else {
+				// Generate Koi
+			}
+		}
+		if (!inited)
+			inited = true;
+	}
+
+	public void DeactivateHole(bool isCatched = false) {
+		if (!isCatched) {
+
+		} else {
+
+		}
+
+		amIActive = false;
+		nextGenTimer = Random.Range (timerMin, timerMax);
+		if (timerMin >= 1.0f) {
+			timerMin -= 0.5f;
+			timerMax -= 1.0f;
+		}
+
+
+	}
+}
